c2cd3fdd7f75673e6db27a475d70bff3e9948a90
JoseJuniorTK <69799917+JoseJuniorTK@users.noreply.github.com>
2020-08-24 10:16:06 -0300

    Update Prototype.py

--- Prototype.py
+++ Prototype.py
@@ -27,6 +27,6 @@ sizes = [a[1], a[0]]
 fig1, ax1 = plt.subplots()
 ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
         shadow=True, startangle=90)
-ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+ax1.axis('equal') 
 
 plt.show()
